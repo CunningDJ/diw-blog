d9fb2ceb0360b0c44aadcef1d9f1a1caaa536a76
David Cunningham <david7789@gmail.com>
2018-06-06 20:00:41 -0400

    Dabbling in Web - first commit.

---
+++ public/js/tech-blog.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var $mainBody = $('#main-body');
+
+var $mainNav = $('#main-nav');
+var navHeight = $mainNav.height();
+
+var NAV_BODY_MARGIN = 25;
+
+// scrolling body-nav offset
+$mainBody.offset({ top: navHeight + NAV_BODY_MARGIN });
